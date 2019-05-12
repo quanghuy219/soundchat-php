d081718fd043874104a1301b539e275c328eac60
Minh Nguyen <minhtoarsenal@gmail.com>
2019-05-12 11:38:15 +0700

    modify to Huy's

--- app/Models/RoomParticipant.php
+++ app/Models/RoomParticipant.php
@@ -2,30 +2,29 @@
 
 namespace App\Models;
 
-use App\Utils\Constants\MediaStatus;
 
 use App\Utils\Constants\ParticipantStatus;
 
 class RoomParticipant extends BaseModel
 {
     protected $table = 'room_participants';
-
-    protected $primaryKey = ['user_id', 'room_id'];
-
-    protected $attributes = [
-        'media_status' => MediaStatus::VOTING,
-        'status' => ParticipantStatus::IN
+    protected $fillable = [
+        'user_id', 'room_id', 'status'
     ];
-
-    protected $fillable = ['user_id', 'room_id'];
-    protected $hidden = ['created', 'updated', 'media_status', 'status'];
-
-    public function user(){
-        $this->morphTo();
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'status' => ParticipantStatus::IN,
+    ];
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
     }
-
-    public function room(){
-        $this->morphTo();
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
-
 }
