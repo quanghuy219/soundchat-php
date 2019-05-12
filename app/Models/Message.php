28a0b8921eaa75531af0fd4cfcf9e457fc609f80
Huy Pham <p.q.huy97@gmail.com>
2019-05-12 13:39:04 +0700

    Add new message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,4 +6,26 @@ namespace App\Models;
 class Message extends BaseModel
 {
     protected $table = 'messages';
+
+    protected $fillable = [
+        'user_id', 'room_id', 'content'
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'status' => 'active',
+    ];
+
+    public function room() {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
