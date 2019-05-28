b7f48f0140f63deb4db0ccfc2b763f46138ff5ea
Huy Pham <p.q.huy97@gmail.com>
2019-05-29 00:07:50 +0700

    Change video_id to id in event data

--- app/Events/DownVote.php
+++ app/Events/DownVote.php
@@ -9,14 +9,14 @@ use App\Utils\Constants\PusherEvent;
 class DownVote extends BaseEvent
 {
     public $user_name; 
-    public $video_id;
+    public $vid;
     public $total_vote; 
 
     public function __construct($room_id, $user, $video)
     {
         $this->room_id = $room_id;
         $this->user_name = $user->getAttribute('name');
-        $this->video_id = $video->getAttribute('id');
+        $this->id = $video->getAttribute('id');
         $this->total_vote = $video->getAttribute('total_vote');
     }
 
